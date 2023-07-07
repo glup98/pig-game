df73ed4b5519d87291b5338d106c1d006f4781a4
glup98 <augustomendoza1998@gmail.com>
2023-07-07 16:34:30 -0400

    finish game

--- script.js
+++ script.js
@@ -1,10 +1,92 @@
 "use strict";
 
 //Seleccion de elementos
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const scoreEl0 = document.querySelector("#score--0");
 const scoreEl1 = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
-scoreEl0.textContent = 0;
-scoreEl1.textContent = 0;
-diceEl.classList.add("hidden");
+let score, currentScore, activePlayer, playing;
+
+//Iniciando Condiciones
+const init = function () {
+  score = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  scoreEl0.textContent = 0;
+  scoreEl1.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+
+init();
+
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+}
+
+//Funcionalidad de los dados
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    //1. Generando numero aleatorio para el dado
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+    //2. Monstrando el dado
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+    //3. Verificar si el dado esta en "1"
+    if (dice !== 1) {
+      //Añadir dado a puntuacion actual
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //si es verdad cambiar al proximo jugador
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    //1. Añadir "score" al jugador activo
+    score[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+    //2.Verificar si el score del jugador es mayo a 100
+    if (score[activePlayer] >= 100) {
+      //Terminar el juego
+      playing = false;
+      diceEl.classList.add("hidden");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      //Cambiar al siguiente jugador
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener("click", init);
